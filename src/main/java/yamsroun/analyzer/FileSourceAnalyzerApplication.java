43355959956ba387743301da7857abd66d056aa4
yamsroun <yamsroun@gmail.com>
2023-05-03 23:46:01 +0900

    Initial Commit

---
+++ src/main/java/yamsroun/analyzer/FileSourceAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package yamsroun.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSourceAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileSourceAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yamsroun/analyzer/FileSourceAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package yamsroun.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSourceAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
