edcd7fa91543d48e40f4d853fe806faf766a12fa
yamsroun <yamsroun@gmail.com>
2023-05-14 02:21:01 +0900

    날짜별/일별 Rollout 결과를 Map 만들 때, 날짜와 서비스명을 구분하도록 수정

--- src/main/java/yamsroun/analyzer/rollout/RolloutMessageAnalyzerExecutor.java
+++ src/main/java/yamsroun/analyzer/rollout/RolloutMessageAnalyzerExecutor.java
@@ -8,9 +8,12 @@ import yamsroun.analyzer.rollout.analyzer.RolloutMessageAnalyzer;
 import yamsroun.analyzer.rollout.data.RolloutInfo;
 import yamsroun.analyzer.rollout.data.RolloutType;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -26,27 +29,31 @@ public class RolloutMessageAnalyzerExecutor {
 
     private void printResult() {
         List<RolloutInfo> result = messageAnalyzer.getResult();
-        Map<String, Integer> stats = new LinkedHashMap<>();
+        Map<DateAndServiceName, Integer> stats = new LinkedHashMap<>();
 
         result.forEach(info -> {
-            String serviceName = String.format("%-20s", info.serviceName());
+            String serviceName = info.serviceName();
             LocalDateTime dateTime = info.rolloutDateTime();
-            String imageTag = String.format("%-30s", info.imageTag());
+            String imageTag = info.imageTag();
             String rolloutDateTime = dateTime.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm"));
             RolloutType rolloutType = info.rolloutType();
             String addingInfo = rolloutType.isDefault() ? "" : " -> " + rolloutType;
-            System.out.printf("%s - %s:%s%s%n", rolloutDateTime, serviceName, imageTag, addingInfo);
+            System.out.printf("%s - %-20s:%30s%s%n", rolloutDateTime, serviceName, imageTag, addingInfo);
 
-            String date = dateTime.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
-            String dateAndServiceName = date + ", " + serviceName;
+            DateAndServiceName dateAndServiceName = new DateAndServiceName(dateTime.toLocalDate(), serviceName);
             stats.putIfAbsent(dateAndServiceName, 0);
             Integer count = stats.get(dateAndServiceName);
             stats.put(dateAndServiceName, ++count);
         });
 
         System.out.println("------------------------------");
-        stats.forEach((dateAndServiceName, count) ->
-            System.out.println(dateAndServiceName + ", " + count)
-        );
+        stats.forEach((dateAndServiceName, count) -> {
+            LocalDate date = dateAndServiceName.date();
+            String serviceName = dateAndServiceName.serviceName();
+            System.out.printf("%s, %-20s, %d%n", date, serviceName, count);
+        });
+    }
+
+    record DateAndServiceName(LocalDate date, String serviceName) {
     }
 }
